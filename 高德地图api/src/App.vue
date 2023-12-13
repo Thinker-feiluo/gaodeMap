fd51f3084d0235cd0dbf892369e305b94cd14256
王世杰 <2660016236@qq.com>
2023-12-13 20:41:38 +0800

    "第一次提交,初始化了地图"

---
+++ 高德地图api/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <GaodeMap></GaodeMap>
+</template>
+
+<script setup>
+import GaodeMap from './components/gaodeMap.vue';
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ 高德地图api/src/components/gaodeMap.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- 这里必须用ID 要不然显示不出来地图 -->
+    <div id="container">
+
+    </div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted, shallowRef } from "vue";
+import AMapLoader from "@amap/amap-jsapi-loader";
+
+const map = null
+
+
+onMounted(() => {
+    AMapLoader.load({
+        key: "bcd0f6abbc4666f14ca0cb412d35ee3a",  // 申请的key
+        version: "2.0",                           // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+        plugins: [],                              // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+    }).then((AMap) => {
+        // 设置地图容器id
+        map = new AMap.Map("container", {
+            viewMode: "3D",                       // 是否为3D地图模式
+            zoom: 11,                             // 初始化地图级别
+            center: [116.397428, 39.90923],       // 初始化地图中心点位置
+        })
+    }).catch((e) => {
+        console.log(e)
+    })
+})
+</script>
+
+<style lang="scss" scoped>
+#container {
+    padding: 0px;
+    margin: 0px;
+    width: 100%;
+    height: 800px;
+}
+</style>
